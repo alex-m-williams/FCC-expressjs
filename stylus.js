13ffb59d11cc196cc72c45dc401dd6a1b5f9eb8b
Alex Williams <alexwilliams567@gmail.com>
2017-11-28 22:22:03 +0000

    stylus example

---
+++ stylus.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const stylus = require('stylus');
+
+app.use(stylus.middleware(process.argv[3]));
+app.use(express.static(process.argv[3]));
+app.listen(process.argv[2]);
